ca34ef189bb62198bd7aaaf2bc9ca2bd9443adb6
Henry Jackson <henry.jackson.95@gmail.com>
2019-08-04 17:38:49 -0400

    add ball component

--- src/pong.rs
+++ src/pong.rs
@@ -16,6 +16,11 @@ pub const ARENA_WIDTH: f32 = 100.0;
 pub const PADDLE_HEIGHT: f32 = 16.0;
 pub const PADDLE_WIDTH: f32 = 4.0;
 
+// ball constants
+pub const BALL_VELOCITY_X: f32 = 75.0;
+pub const BALL_VELOCITY_Y: f32 = 50.0;
+pub const BALL_RADIUS: f32 = 2.0;
+
 #[derive(PartialEq, Eq)]
 pub enum Side {
     Left,
@@ -40,6 +45,15 @@ impl Paddle {
 }
 
 impl Component for Paddle {
+    type Storage = DenseVecStorage<Self>;
+}
+
+pub struct Ball {
+    pub velocity: [f32; 2],
+    pub radius: f32,
+}
+
+impl Component for Ball {
     type Storage = DenseVecStorage<Self>;
 }
 
@@ -116,6 +130,29 @@ fn load_sprite_sheet(world: &mut World) -> Handle<SpriteSheet> {
     )
 }
 
+// Initialises a ball in the middle of the arena
+fn initialise_ball(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>) {
+    // Create the starting coords
+    let mut local_transform = Transform::default();
+    local_transform.set_translation_xyz(ARENA_WIDTH / 2.0, ARENA_HEIGHT / 2.0, 0.0);
+
+    // Assign the sprite for the ball
+    let sprite_render = SpriteRender {
+        sprite_sheet: sprite_sheet_handle,
+        sprite_number: 1, // ball is the second sprite on the sprite sheet
+    };
+
+    world
+        .create_entity()
+        .with(sprite_render)
+        .with(Ball {
+            radius: BALL_RADIUS,
+            velocity: [BALL_VELOCITY_X, BALL_VELOCITY_Y],
+        })
+        .with(local_transform)
+        .build();
+}
+
 // define our game state
 pub struct Pong;
 
@@ -123,11 +160,13 @@ impl SimpleState for Pong {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let world = data.world;
 
+        world.register::<Ball>(); // temp until system exists
+
         // Load sprite sheet necessary to render graphics
         let sprite_sheet_handle = load_sprite_sheet(world);
         
-        initialise_camera(world);
-
+        initialise_ball(world, sprite_sheet_handle.clone());
         initialise_paddles(world, sprite_sheet_handle);
+        initialise_camera(world);
     }
 }
